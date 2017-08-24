8d76cfcdb68c4f40bc5cd7e7c4c47a2e82354465
home-coder <one_face@sina.com>
2017-08-24 22:16:00 +0800

    增加kl文件处理主要逻辑代码

--- text_edit.sh
+++ text_edit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 function write_mk_file() 
 {
 	#test
@@ -21,7 +21,7 @@ function write_mk_file()
 function write_txt_file()
 {
 	param_file="external_product.txt"
-	param_key="BOX-4"
+	param_key="BOX"
 	param_value="迪优美特222=东莞市智而浦实业有限公司=4007772628=3375381074@qq.com"
 	echo $param_file $param_key $param_value
 	
@@ -36,15 +36,17 @@ function write_txt_file()
 
 function write_kl_file()
 {
-	param_file="customer_ir_df00.kl"
+	param_file="custom_ir_1044.kl"
 	param_key="10"
-	param_value="BACK"
+	param_value="MENU"
 
-	while read line; do
-		arry=($(echo $line | awk -F ' ' '{for(i=1; i <=NF; i++) print $i}'))
-		echo ${arry[*]}
-	done < $param_file
-
+	#sed -i '/'[[:space:]]$param_key[[:space:]]'/s/.*/'key' '$param_key'    '$param_value'/g' $param_file
+	cat $param_file | awk 'BEGIN{ FS=" ";} {
+		if ($2=='$param_key') {
+			gsub($3,"'$param_value'")
+		}
+		print $0 > "'$param_file'"
+	}'
 }
 
 #write_mk_file
