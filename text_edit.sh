7b5e0c2b7f3cfc5de496adb2d3d75c0f6f83d49a
jiangxiujie <jiangxiujie@cantv.cn>
2017-08-25 11:38:49 +0800

    完善kl文件的写入函数

--- text_edit.sh
+++ text_edit.sh
@@ -34,27 +34,54 @@ function write_txt_file()
 	fi
 }
 
+#
+#@PARAM: 1:custom_ir_1044.kl
+#		 2:128
+#		 3:POWER   WAKE
+#@FUNCTION: 根据参数param_key判断是否是新的num，如果不是则替换key_code，如果是则追加到kl文件末尾
+#
 function write_kl_file()
 {
 	param_file="custom_ir_1044.kl"
-	param_key="10"
-	param_value="MENU"
+	param_key="128"
+	param_value="POWER   WAKE"
 
-	#sed -i '/'[[:space:]]$param_key[[:space:]]'/s/.*/'key' '$param_key'    '$param_value'/g' $param_file
-	cat $param_file | awk 'BEGIN{ FS=" ";} {
-		if ($2=='$param_key') {
-			gsub($3,"'$param_value'")
-		}
-		print $0 > "'$param_file'"
-	}'
+	local num
+	key_num=$(cat $param_file|awk '{for(i=1; i<=NF; i++) print $i}')
+	for num in ${key_num[*]}; do
+		if [[ $num == $param_key ]]; then
+			flag=1
+			break
+		else
+			flag=0
+		fi
+	done
+
+	if [[ $flag -eq 0 ]]; then
+		add_kv="key $param_key    $param_value"
+		echo $add_kv >> $param_file
+	else
+		sed_value=""
+		value_arry=$(echo $param_value|awk '{for(i=1; i<=NF; i++) print $i}')
+		for value in ${value_arry[*]}; do
+			if [[ -z $sed_value ]]; then
+				sed_value=$value
+			else
+				sed_value="$sed_value,,,,$value"
+			fi
+		done
+
+		sed -i '/'[[:space:]]$param_key[[:space:]]'/s/.*/'key' '$param_key'    '$sed_value'/g'  $param_file
+		sed -i 's/,,,,/\ \ \ \ /g' $param_file
+	fi
 }
 
 function write_efex_file()
 {
-
+	echo -----
 }
 
 #write_mk_file
 #write_txt_file
-#write_kl_file
-write_efex_file
+write_kl_file
+#write_efex_file
